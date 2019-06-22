57de29e26a0b1e37a6fef8a1a902e592216cc369
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-06-22 13:57:35 +0530

    testing sockets

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 require('./config/config')
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, () => {
+io.on('connection', (socket) => {
+	console.log('New user connected');
+	
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
+
+server.listen(process.env.PORT, () => {
 	console.log(`Listening on port ${process.env.PORT}`);
 });
