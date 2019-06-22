3057ff89af968c95dccf90602631fcaf670b5c32
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-06-22 19:39:50 +0530

    added geolocation stuff

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-const { generateMessage } = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 app.use(express.static(publicPath));
 
@@ -24,8 +24,13 @@ io.on('connection', (socket) => {
 
 	socket.on('createMessage', (message, callback) => {
 		console.log('createMessage', message);
+
 		io.emit('newMessage', generateMessage(message.from,message.text));
 		callback('This is from the server');
+	});
+
+	socket.on('createLocationMessage', (coords) => {
+		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
 	});
 
 	socket.on('disconnect', () => {
