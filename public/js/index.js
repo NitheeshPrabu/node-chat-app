1a3c72b970ca7baf31759e8a97c889d3b67a2227
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-06-22 19:51:23 +0530

    add css for chat

--- public/js/index.js
+++ public/js/index.js
@@ -32,6 +32,8 @@ socket.on('newLocationMessage', function(message) {
 	jQuery('#messages').append(li);
 });
 
+var messageTextBox = jQuery('[name=message]');
+
 jQuery('#message-form').on('submit', function(e) {
 	e.preventDefault();
 
@@ -39,7 +41,8 @@ jQuery('#message-form').on('submit', function(e) {
 		from: 'User',
 		text: jQuery('[name=message]').val()
 	}, function() {
-
+		messageTextBox.val('');
+		messageTextBox.focus();
 	});
 });
 
@@ -49,12 +52,16 @@ locationButton.on('click', function(e) {
 		return alert('Geolocation not supported by your browser.');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(function(position) {
+		locationButton.removeAttr('disabled').text('Send location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, function() {
+		locationButton.removeAttr('disabled').text('Send location');
 		alert('Unable to fetch location');
 	});
 });
